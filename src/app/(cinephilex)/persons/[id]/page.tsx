7694c1d076b440bd5cdd8f14247876996d4b1241
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-05 17:02:22 -0300

    :sparkles: feat: Criando rotas de persons

---
+++ src/app/(cinephilex)/persons/[id]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Person(props: any) {
+  return <div>{JSON.stringify(props)}</div>;
+}
---
+++ src/app/(cinephilex)/persons/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Persons() {
+  return (
+    <div>Persons</div>
+  )
+}
+
