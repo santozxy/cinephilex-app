ce9cdd808b11d2e3a1ae05a80c5fcbc348477bd5
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-05 17:03:07 -0300

    :bricks: config: Ajustando configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["tmdb.org", "themoviedb.org"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
