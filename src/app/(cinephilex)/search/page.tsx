e876df91b2a621e08bb065bc538e1ba63fd95d7b
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-05 17:59:50 -0300

    :truck: rename: Alterando nomenclatura para legibilidade

--- src/app/(cinephilex)/search/page.tsx
+++ src/app/(cinephilex)/search/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
-import React, { Suspense } from "react";
-import ListCards from "@/components/list-search-cards";
+import React from "react";
+import { ListSearchCards } from "@/components/list-search-cards";
 import { SearchDTO } from "@/service/search/searchDTO";
 import { searchProducts } from "@/service/search/api";
 
@@ -21,7 +21,7 @@ export default async function Search({ searchParams }: SearchProps) {
       <h1 className="text-2xl font-medium mb-10">
         Resultado da busca por: <strong> {query}</strong>
       </h1>
-      <ListCards data={data} />
+      <ListSearchCards data={data} />
     </div>
   );
 }
