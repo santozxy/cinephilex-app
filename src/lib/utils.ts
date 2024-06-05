9ed0a6107eaf3c7fa546b1db82ce1c1e267b4aa5
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-05 17:15:13 -0300

    :sparkles: feat: Criando arquivo utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import Autoplay from "embla-carousel-autoplay";
+import React from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const originalImageURL = process.env.NEXT_PUBLIC_IMG_BASE_URL;
+export const resizeImageURL = process.env.NEXT_PUBLIC_IMG_JPG_BASE_URL;
