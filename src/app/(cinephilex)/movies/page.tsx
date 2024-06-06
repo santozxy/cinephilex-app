ed0f62b669b8736ac509c6daac75fe13520cc123
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-06 16:52:38 -0300

    :construction: progress: Criando page de movies

--- src/app/(cinephilex)/movies/page.tsx
+++ src/app/(cinephilex)/movies/page.tsx
@@ -1,5 +1,35 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { originalImageURL } from "@/lib/utils";
+import {
+  getBrazilianPopularMovies,
+  getNowPlayingMovies,
+  getPopularMovies,
+  getTrendingDayMovies,
+} from "@/service/movies/api";
+import Image from "next/image";
 import React from "react";
+import { CarouselAutoplay } from "../../../components/carousel-autoplay";
+import ListCards from "@/components/list-cards";
+import Link from "next/link";
+import { getTopRatedSeries, getTrendingDaySeries } from "@/service/series/api";
 
-export default function Movies() {
-  return <div>Movies</div>;
+export default async function Movies() {
+  const trendingMovies = await getTrendingDayMovies();
+
+  return (
+    <main className="flex flex-col gap-8 justify-center items-center">
+      <ListCards
+        path="/movies/"
+        titleSection="Em Alta Agora"
+        data={trendingMovies}
+        type="movie"
+      />
+    </main>
+  );
 }
