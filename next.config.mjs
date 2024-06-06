0ac48309dc390d7b55e515c9a8d842ab7cdba3fa
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-06 16:48:18 -0300

    :bricks: config: Atualizar configuração de imagens para lazy loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["tmdb.org", "themoviedb.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/w500/*",
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/original/*",
+      },
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,
