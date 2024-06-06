bd6c464533553c0bc7cb4adf4b60470a4fc6c2f6
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-06 16:50:06 -0300

    :sparkles: feat: add loading na page Search

--- src/app/(cinephilex)/search/loading.tsx
+++ src/app/(cinephilex)/search/loading.tsx
@@ -3,9 +3,9 @@ import { Skeleton } from "@/components/skeleton";
 export default function SearchLoading() {
   return (
     <section className="flex flex-wrap gap-4 justify-center">
-      {[...Array(15)].map((_, index) => (
+      {[...Array(14)].map((_, index) => (
         <div key={index} className="">
-          <Skeleton className="w-[15rem] h-[20rem]" />
+          <Skeleton className="w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md shadow-lg shadow-black/30" />
         </div>
       ))}
     </section>
