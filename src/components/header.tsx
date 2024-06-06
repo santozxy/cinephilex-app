821865828bfee7e0383223d16d47fb3112c67ec5
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-06 16:50:43 -0300

    :sparkles: feat: add logo quando estiver no mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,39 +12,44 @@ export default function Header() {
         <h1 className="text-2xl max-sm:hidden leading-relaxed text-primary font-semibold">
           CinephileX
         </h1>
-      
       </nav>
       <nav className="flex gap-8 justify-center items-center ">
-      <Link href="/" className="py-2 no-underline hover:underline sm:hidden">
+        <Link href="/" className="py-2 no-underline hover:underline sm:hidden">
           <div className="flex items-center justify-center gap-3">
-          <Image className="" src="/logo.png" alt="CinephileX" width={24} height={24} />
+            <Image
+              className=""
+              src="/logo.png"
+              alt="CinephileX"
+              width={24}
+              height={24}
+            />
           </div>
         </Link>
-     
+
         <Link href="/" className="py-2 no-underline hover:underline">
           <div className="flex items-center justify-center gap-3">
             <Home size={24} className={`text-lg text-primary`} />
-            <p className={`text-lg max-[450px]:hidden`}>Home</p>
+            <p className={`text-lg max-[450px]:hidden`}>Início</p>
           </div>
         </Link>
 
         <Link href="/movies" className="py-2 no-underline hover:underline">
           <div className="flex items-center justify-center gap-3">
             <Film size={24} className={`text-lg text-primary`} />
-            <p className={`text-lg max-[450px]:hidden`}>Movies</p>
+            <p className={`text-lg max-[450px]:hidden`}>Filmes</p>
           </div>
         </Link>
 
         <Link href="/series" className="py-2 no-underline hover:underline">
           <div className="flex items-center justify-center gap-3">
             <Tv size={24} className={`text-lg text-primary`} />
-            <p className={`text-lg max-[450px]:hidden`}>Series</p>
+            <p className={`text-lg max-[450px]:hidden`}>Séries</p>
           </div>
         </Link>
         <Link href="/persons" className="py-2 no-underline hover:underline">
           <div className="flex items-center justify-center gap-3">
             <LucideUsers size={24} className={`text-lg text-primary`} />
-            <p className={`text-lg max-[450px]:hidden`}>Persons</p>
+            <p className={`text-lg max-[450px]:hidden`}>Pessoas</p>
           </div>
         </Link>
       </nav>
