8336d5bb88682ad44ffdaeb691d578d6c7b45382
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-06 16:47:29 -0300

    :sparkles: feat: Adicionando - listagem de filmes populares; - Listagem de Series Melhor avalidadas - Listagem de Séries Em Alta Agora

--- src/app/(cinephilex)/(home)/loading.tsx
+++ src/app/(cinephilex)/(home)/loading.tsx
@@ -1,13 +1,97 @@
-import { CarouselAutoplay } from "@/components/carousel-autoplay";
 import { Skeleton } from "@/components/skeleton";
-import { CarouselItem } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Film, Tv } from "lucide-react";
 import React from "react";
 
 export default function HomeLoading() {
   return (
-    <main className="flex justify-center items-center">
-      <div className="mx-10">
-        <Skeleton className="w-full md:h-[32rem] lg:h-[40rem] max-sm:h-64" />
+    <main className="flex flex-col gap-8 justify-center items-center mx-10">
+      <Skeleton className="w-full md:h-[32rem] lg:h-[40rem] max-sm:h-64" />
+      <div className="flex gap-2 items-center">
+        <Film size={24} className="text-primary" />
+        <h1 className="text-2xl font-bold my-6 ">Filmes Populares</h1>
+      </div>
+      <div className="flex flex-wrap gap-2 justify-center">
+        <Carousel>
+          <CarouselContent>
+            {[1, 2, 3, 4, 5, 6, 7].map((item) => (
+              <CarouselItem
+                key={item}
+                className="basis-auto justify-center items-center"
+              >
+                <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md shadow-lg shadow-black/30 hover:ease-in-out" />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselNext />
+          <CarouselPrevious />
+        </Carousel>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Tv size={24} className="text-primary" />
+        <h1 className="text-2xl font-bold my-6 ">Em Alta Agora</h1>
+      </div>
+      <div className="flex flex-wrap gap-2 justify-center">
+        <Carousel>
+          <CarouselContent>
+            {[1, 2, 3, 4, 5, 6, 7].map((item) => (
+              <CarouselItem
+                key={item}
+                className="basis-auto justify-center items-center"
+              >
+                <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md shadow-lg shadow-black/30 hover:ease-in-out" />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselNext />
+          <CarouselPrevious />
+        </Carousel>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Film size={24} className="text-primary" />
+        <h1 className="text-2xl font-bold my-6 ">Filmes Populares no Brasil</h1>
+      </div>
+      <div className="flex flex-wrap gap-2 justify-center">
+        <Carousel>
+          <CarouselContent>
+            {[1, 2, 3, 4, 5, 6, 7].map((item) => (
+              <CarouselItem
+                key={item}
+                className="basis-auto justify-center items-center"
+              >
+                <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md shadow-lg shadow-black/30 hover:ease-in-out" />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselNext />
+          <CarouselPrevious />
+        </Carousel>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Tv size={24} className="text-primary" />
+        <h1 className="text-2xl font-bold my-6 ">Filmes Populares no Brasil</h1>
+      </div>
+      <div className="flex flex-wrap gap-2 justify-center">
+        <Carousel>
+          <CarouselContent>
+            {[1, 2, 3, 4, 5, 6, 7].map((item) => (
+              <CarouselItem
+                key={item}
+                className="basis-auto justify-center items-center"
+              >
+                <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md shadow-lg shadow-black/30 hover:ease-in-out" />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselNext />
+          <CarouselPrevious />
+        </Carousel>
       </div>
     </main>
   );
--- src/app/(cinephilex)/(home)/page.tsx
+++ src/app/(cinephilex)/(home)/page.tsx
@@ -1,46 +1,74 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+import { CarouselItem } from "@/components/ui/carousel";
 import { originalImageURL } from "@/lib/utils";
-import { getNowPlayingMovies, getPopularMovies } from "@/service/movies/api";
+import {
+  getBrazilianPopularMovies,
+  getNowPlayingMovies,
+  getPopularMovies,
+} from "@/service/movies/api";
 import Image from "next/image";
 import React from "react";
 import { CarouselAutoplay } from "../../../components/carousel-autoplay";
 import ListCards from "@/components/list-cards";
 import Link from "next/link";
-
+import { getTopRatedSeries, getTrendingDaySeries } from "@/service/series/api";
 
 export default async function Home() {
   const nowPlayingMovies = await getNowPlayingMovies();
   const popularMovies = await getPopularMovies();
+  const brazilianPopularMovies = await getBrazilianPopularMovies();
+  const TopRatedSeries = await getTopRatedSeries();
+  const trendingSeries = await getTrendingDaySeries();
+
   return (
     <main className="flex flex-col gap-8 justify-center items-center">
-      <CarouselAutoplay>
-        {nowPlayingMovies.results.map((movie) => (
-          <CarouselItem key={movie.id} className="basis-auto shadow-xl shadow-black/40">
-            <Link href={`/movies/${movie.id}`}>
-              <Image
-                unoptimized
-                priority
-                src={`${originalImageURL}${movie.backdrop_path}`}
-                alt={movie.title}
-                width={1200}
-                height={512}
-                quality={80}
-                className="object-contain max-sm:h-64 bg-center rounded-md shadow-lg"
-              />
-            </Link>
-          </CarouselItem>
-        ))}
+      <CarouselAutoplay titleSection="Em Cartaz">
+        {nowPlayingMovies.results.map((movie) => {
+          if (!movie.backdrop_path) {
+            return null;
+          }
+          return (
+            <CarouselItem
+              key={movie.id}
+              className="basis-auto shadow-xl shadow-black/40"
+            >
+              <Link href={`/movies/${movie.id}`}>
+                <Image
+                  src={`${originalImageURL}${movie.backdrop_path}`}
+                  alt={movie.title}
+                  width={1280}
+                  height={720}
+                  loading="lazy"
+                  quality={70}
+                  className=" max-sm:h-64 h-[35rem] bg-center w-[60rem] max-sm:w-full rounded-md shadow-lg"
+                />
+              </Link>
+            </CarouselItem>
+          );
+        })}
       </CarouselAutoplay>
       <ListCards
         path="/movies/"
-        titleSection="Popular Movies"
+        titleSection="Filmes Populares"
         data={popularMovies}
+        type="movie"
+      />
+      <ListCards
+        path="/series/"
+        titleSection="Em Alta Agora"
+        data={trendingSeries}
+        type="tv"
+      />
+      <ListCards
+        path="/movies/"
+        titleSection="Filmes Populares no Brasil"
+        data={brazilianPopularMovies}
+        type="movie"
+      />
+      <ListCards
+        path="/series/"
+        titleSection="Séries Melhor Avaliadas"
+        data={TopRatedSeries}
+        type="tv"
       />
     </main>
   );
