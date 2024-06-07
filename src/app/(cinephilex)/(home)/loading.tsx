af3b6f39176005e3a49daf65b013448a443789fe
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-07 14:06:42 -0300

    :lipstick: feat: Melhorando o loading

--- src/app/(cinephilex)/(home)/loading.tsx
+++ src/app/(cinephilex)/(home)/loading.tsx
@@ -8,97 +8,100 @@ import {
 } from "@/components/ui/carousel";
 import { Film, Popcorn, Tv } from "lucide-react";
 import React from "react";
-
 export default function HomeLoading() {
   return (
-    <main className="flex flex-col gap-8 justify-center items-center mx-10">
+    <main className="flex flex-col justify-center items-center">
       <div className="flex flex-col mx-10 w-full">
+        <Skeleton className="max-sm:h-64 h-[40rem] w-full rounded-md shadow-lg" />
+      </div>
+      <section className="mx-10">
         <div className="flex gap-2 items-center">
-          <Popcorn size={24} className="text-primary" />
-          <h1 className="text-lg font-bold my-6 ">Em Cartaz</h1>
+          <Film size={24} className="text-primary" />
+          <h1 className="text-lg font-bold my-6 ">Em Alta Agora</h1>
         </div>
-        <Skeleton className="max-sm:h-64 h-[35rem] w-full rounded-md shadow-lg" />
-      </div>
-      <div className="flex gap-2 items-center">
-        <Film size={24} className="text-primary" />
-        <h1 className="text-2xl font-bold my-6 ">Filmes Populares</h1>
-      </div>
-      <div className="flex flex-wrap gap-2 justify-center">
-        <Carousel>
-          <CarouselContent>
-            {[1, 2].map((item) => (
-              <CarouselItem
-                key={item}
-                className="basis-auto shadow-xl shadow-black/40"
-              >
-                <Skeleton className="max-sm:h-64 h-[35rem] bg-center w-[60rem] max-sm:w-full rounded-md shadow-lg" />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselNext />
-          <CarouselPrevious />
-        </Carousel>
-      </div>
-      <div className="flex gap-2 items-center">
-        <Tv size={24} className="text-primary" />
-        <h1 className="text-2xl font-bold my-6 ">Em Alta Agora</h1>
-      </div>
-      <div className="flex flex-wrap gap-2 justify-center">
-        <Carousel>
-          <CarouselContent>
-            {[1, 2, 3, 4, 5, 6, 7].map((item) => (
-              <CarouselItem
-                key={item}
-                className="basis-auto justify-center items-center"
-              >
-                <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md shadow-lg shadow-black/30 hover:ease-in-out" />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselNext />
-          <CarouselPrevious />
-        </Carousel>
-      </div>
-      <div className="flex gap-2 items-center">
-        <Film size={24} className="text-primary" />
-        <h1 className="text-2xl font-bold my-6 ">Filmes Populares no Brasil</h1>
-      </div>
-      <div className="flex flex-wrap gap-2 justify-center">
-        <Carousel>
-          <CarouselContent>
-            {[1, 2, 3, 4, 5, 6, 7].map((item) => (
-              <CarouselItem
-                key={item}
-                className="basis-auto justify-center items-center"
-              >
-                <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md shadow-lg shadow-black/30 hover:ease-in-out" />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselNext />
-          <CarouselPrevious />
-        </Carousel>
-      </div>
-      <div className="flex gap-2 items-center">
-        <Tv size={24} className="text-primary" />
-        <h1 className="text-2xl font-bold my-6 ">Filmes Populares no Brasil</h1>
-      </div>
-      <div className="flex flex-wrap gap-2 justify-center">
-        <Carousel>
-          <CarouselContent>
-            {[1, 2, 3, 4, 5, 6, 7].map((item) => (
-              <CarouselItem
-                key={item}
-                className="basis-auto justify-center items-center"
-              >
-                <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md shadow-lg shadow-black/30 hover:ease-in-out" />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselNext />
-          <CarouselPrevious />
-        </Carousel>
-      </div>
+        <div className="flex flex-wrap gap-2 justify-center">
+          <Carousel>
+            <CarouselContent>
+              {[1, 2, 3, 4, 5, 6].map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className="basis-auto justify-center items-center"
+                >
+                  <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md" />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselNext />
+            <CarouselPrevious />
+          </Carousel>
+        </div>
+      </section>
+      <section className="mx-10">
+        <div className="flex gap-2 items-center">
+          <Tv size={24} className="text-primary" />
+          <h1 className="text-2xl font-bold my-6 ">Melhores Avaliados</h1>
+        </div>
+        <div className="flex flex-wrap gap-2 justify-center">
+          <Carousel>
+            <CarouselContent>
+              {[1, 2, 3, 4, 5, 6].map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className="basis-auto justify-center items-center"
+                >
+                  <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md" />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselNext />
+            <CarouselPrevious />
+          </Carousel>
+        </div>
+      </section>
+      <section className="mx-10">
+        <div className="flex gap-2 ">
+          <Film size={24} className="text-primary" />
+          <h1 className="text-2xl font-bold my-6 ">Terror</h1>
+        </div>
+        <div className="flex flex-wrap gap-2 justify-center">
+          <Carousel>
+            <CarouselContent>
+              {[1, 2, 3, 4, 5, 6].map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className="basis-auto justify-center items-center"
+                >
+                  <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md" />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselNext />
+            <CarouselPrevious />
+          </Carousel>
+        </div>
+      </section>
+      <section className="mx-10">
+        <div className="flex gap-2 items-center">
+          <Tv size={24} className="text-primary" />
+          <h1 className="text-2xl font-bold my-6 ">Animação</h1>
+        </div>
+        <div className="flex flex-wrap gap-2 justify-center">
+          <Carousel>
+            <CarouselContent>
+              {[1, 2, 3, 4, 5, 6].map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className="basis-auto justify-center items-center"
+                >
+                  <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md" />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselNext />
+            <CarouselPrevious />
+          </Carousel>
+        </div>
+      </section>
     </main>
   );
 }
