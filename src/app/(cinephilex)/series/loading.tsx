de5f6d6b2401d5e8db5377f0b9d7460fa66acc6a
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-10 10:52:02 -0300

    :lipstick: feat: Melhorando o loading

--- src/app/(cinephilex)/series/loading.tsx
+++ src/app/(cinephilex)/series/loading.tsx
@@ -18,7 +18,7 @@ export default function MoviesLoading() {
       <section className="mx-10">
         <div className="flex gap-2 items-center">
           <Film size={24} className="text-primary" />
-          <h1 className="text-lg font-bold my-6 ">Em Alta Agora</h1>
+          <h1 className="text-lg font-bold my-6 ">Comédia</h1>
         </div>
         <div className="flex flex-wrap gap-2 justify-center">
           <Carousel>
@@ -40,7 +40,7 @@ export default function MoviesLoading() {
       <section className="mx-10">
         <div className="flex gap-2 items-center">
           <Tv size={24} className="text-primary" />
-          <h1 className="text-2xl font-bold my-6 ">Melhores Avaliados</h1>
+          <h1 className="text-2xl font-bold my-6 ">Em Alta Agora</h1>
         </div>
         <div className="flex flex-wrap gap-2 justify-center">
           <Carousel>
@@ -62,7 +62,29 @@ export default function MoviesLoading() {
       <section className="mx-10">
         <div className="flex gap-2 ">
           <Film size={24} className="text-primary" />
-          <h1 className="text-2xl font-bold my-6 ">Terror</h1>
+          <h1 className="text-2xl font-bold my-6 ">Mistério</h1>
+        </div>
+        <div className="flex flex-wrap gap-2 justify-center">
+          <Carousel>
+            <CarouselContent>
+              {[1, 2, 3, 4, 5, 6].map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className="basis-auto justify-center items-center"
+                >
+                  <Skeleton className=" w-[12rem] max-sm:h-60 max-sm:w-36 lg:h-[17rem] rounded-md" />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselNext />
+            <CarouselPrevious />
+          </Carousel>
+        </div>
+      </section>
+      <section className="mx-10">
+        <div className="flex gap-2 items-center">
+          <Tv size={24} className="text-primary" />
+          <h1 className="text-2xl font-bold my-6 ">Melhor Avaliados</h1>
         </div>
         <div className="flex flex-wrap gap-2 justify-center">
           <Carousel>
