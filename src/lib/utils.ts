fb4616aa7d1c9e81dc3cdff4c2c5e7f3d716fc7b
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-10 11:21:17 -0300

    :sparkles: feat: criando um utils para generos

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,4 @@
 import { type ClassValue, clsx } from "clsx";
-import Autoplay from "embla-carousel-autoplay";
-import React from "react";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
